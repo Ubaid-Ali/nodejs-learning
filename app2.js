c8bef9947301c635848ab0f0240012636e5e3878
Ubaid Ali <syedubaidali2020@gmail.com>
2021-11-24 04:34:07 +0500

    Initial commit

---
+++ app2.js
@@ -0,0 +1,28 @@
+// - - - - - - - - - -
+// Example 1
+// Create Function
+
+// function sayHello(name) {
+//     console.log(`Hello`, name);
+// };
+// sayHello("Ubaid");
+
+// - - - - - - - - - -
+// Example 2
+// Create Variable
+
+// var msg = "Thank you for everything!";
+// console.log(`msg is`, msg)
+// console.log(global.msg); undefined
+
+// - - - - - - - - - -
+// Example 3
+// Module
+
+// console.log(`Module`, module)
+// Returns Json Oject in Terminal
+
+// - - - - - - - - - -
+// Example 4
+
+
