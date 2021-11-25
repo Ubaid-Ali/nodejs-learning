4b25514cca86cafb73e2db14f52bacfde26a2d46
Ubaid Ali <syedubaidali2020@gmail.com>
2021-11-25 21:31:14 +0500

    Update step2-variable.js

--- step2-variable.js
+++ step2-variable.js
@@ -3,11 +3,12 @@ var msg = "This is Message!";
 
 console.log(`msg is`, msg) // Working
 
-// console.log(global.msg); // Undefined
+// console.log(msg); // Undefined
 
 // console.log("window.msg", window.msg);
 // ReferenceError: window is not defined
 
+// Global
 // global.console.log(`global.console`, msg) // Working
 
 // Working
