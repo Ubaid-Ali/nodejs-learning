ae3ade21138a17ae7af15d2db9ff99fc37f8cd95
Ubaid Ali <syedubaidali2020@gmail.com>
2021-11-26 05:03:14 +0500

    Create step4-loading-module.js

---
+++ step4-loading-module.js
@@ -0,0 +1,11 @@
+const myFunction = require("./step3-export-module");
+
+
+// console.log(logger)                          //true
+// myFunction("Message from step4")             //true
+
+
+console.log(`myFunction`, myFunction)
+
+
+
