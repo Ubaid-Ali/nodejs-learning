87250757cf8d169cb28da1d1a223f32be7461618
Ubaid Ali <syedubaidali2020@gmail.com>
2021-12-08 04:33:43 +0500

    Create step5-module-wrapper-function.js

---
+++ step5-module-wrapper-function.js
@@ -0,0 +1,12 @@
+// (function background_example(exports, require, module, __filename, __dirname) {
+    // example 
+    // working
+// })
+
+console.log(`exports`, exports)
+console.log(`require`, require)
+console.log(`module`, module)
+console.log(`__filename`, __filename)
+console.log(`__dirname`, __dirname)
+
+// module.exports.myFunction = myFunction;
